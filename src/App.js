6be0af710c17fce87e5e787cd584c10506030e2f
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-15 11:27:18 -0400

    added image details and input box for search

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ function App() {
     return (
         <div className="App">
             <h1>Image Repository</h1>
+            <form>
+                <input className="search-box" type="text" placeholder="Search..." />
+            </form>
+            <br/>
             {data.map(item => {
                 return (
-                    <img className="image-container" src={require(`./images/${item.file_name}`).default} alt="image goes here"/>
+                    <>
+                        <img className="image-container" src={require(`./images/${item.file_name}`).default} alt="image goes here"/>
+                        <p>Title: {item.name}</p>
+                        <p>Filename: {item.file_name}</p>
+                        <p>Description: {item.description}</p>
+                    </>
                 );
             })}
         </div>
