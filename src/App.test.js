614cc4992f786bf4f4248e0fff020adc39904a50
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-16 14:28:44 -0400

    updated first test name

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // testing title says Image Repository
-test('renders learn react link', () => {
+test('renders App component and checks title accuracy', () => {
   render(<App />);
   const linkElement = screen.getByText(/Image Repository/i);
   expect(linkElement).toBeInTheDocument();
