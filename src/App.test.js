a4be46c0f1a7af109053687e5151bf9ee9d89759
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-16 14:09:45 -0400

    added comments to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// testing title says Image Repository
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Image Repository/i);
