348067baab9a4c1fc9ae09bf3142d68bce86c868
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-16 14:27:30 -0400

    removed test case for enzyme due to driver import issues

--- src/App.test.js
+++ src/App.test.js
@@ -8,10 +8,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/Image Repository/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-describe(<App />, () => {
-  it('renders images name label', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('p').text()).toBe('Title: ');
-  });
-});
