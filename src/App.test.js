a76bb900dea52f7f68ea7053df2a9df80293c418
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-16 14:24:37 -0400

    testing p tag rendering wanted text

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
 // testing title says Image Repository
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Image Repository/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe(<App />, () => {
+  it('renders images name label', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('p').text()).toBe('Title: ');
+  });
 });
