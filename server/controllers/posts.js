9eb993a8e45bc0a7ac957c64d549b5164b4cbba1
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-22 21:48:29 -0400

    created controller functionality to get images

---
+++ server/controllers/posts.js
@@ -0,0 +1,11 @@
+import Post from '../models/Post.js';
+
+export const getImages = (req, res) => {
+    try {
+        const images = await Post.find();
+        res.json(images);
+    }
+    catch (error) {
+        res.json({ message: error);
+    }
+};
