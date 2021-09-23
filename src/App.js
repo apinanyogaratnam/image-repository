fe70b47646ad885c6a7d5441651632fa1d6f85df
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-22 21:56:57 -0400

    added functionality to connect to mongodb

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import './App.css';
 import data from './data.json';
 import { useEffect, useState } from 'react';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send("Home page");
+});
+
+mongoose.connect(
+    process.env.DATABASE_URL, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to database')
+);
+
+app.listen(3000, () => console.log("server started on port 3000"));
 
 function App() {
     const [search, setSearch] = useState('');
